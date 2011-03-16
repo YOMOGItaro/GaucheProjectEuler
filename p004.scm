ce8e6d3ea4ebab728b826b7bd17d00d5fd965325
YOMOGItaro <yomogitaro@gmail.com>
2011-03-16 19:51:49 +0900

    p004 finish

--- p004.scm
+++ p004.scm
@@ -4,45 +4,11 @@
 
 ;; Find the largest palindrome made from the product of two 3-digit numbers.
 
+; 580085
+
+(add-load-path ".")
+(use util.palindromic)
+
 
 (palindrome-from-product-of-two-3-digit)
-(define (palindrome-from-product-of-two-3-digit)
-  (define (pfpot3d-iter pair max)
-    (cond
-     ((and (>= (car pair) 999) (>= (cdr pair) 999))
-      max)
-     ((palindromic? (pair-multi pair))
-      (pfpot3d-iter (pair-next pair) (pair-multi pair)))
-     (else
-      (pfpot3d-iter (pair-next pair) max))))
-  (pfpot3d-iter '(100 . 100) 10201))
-
-
-;(palindromic? 123454321)
-;(palindromic? 123445321)
-(define (palindromic? num)
-  (= (num-reverse num) num))
-
-
-(define (num-reverse num)
-  (define (num-reverse-iter ret pool)
-    (if (= pool 0)
-	ret
-	(num-reverse-iter
-	 (+ (* ret 10) (remainder pool 10))
-	 (quotient pool 10))))
-  (num-reverse-iter 0 num))
-
-
-;(pair-multi '(100 . 101))
-(define (pair-multi pair)
-  (* (car pair) (cdr pair)))
-
-
-;(pair-next '(999 . 100))
-;(pair-next '(998 . 100))
-(define (pair-next pair)
-  (cond
-   ((>= (car pair) 999) (cons 100 (+ (cdr pair) 1)))
-   (else (cons (+ (car pair) 1) (cdr pair)))))
 
---
+++ util/palindromic.scm
@@ -0,0 +1,48 @@
+(define-module util.palindromic
+  (export-all))
+(select-module util.palindromic)
+
+
+;(palindrome-from-product-of-two-3-digit)
+(define (palindrome-from-product-of-two-3-digit)
+  (define (pfpot3d-iter pair max)
+    (cond
+     ((and (>= (car pair) 999) (>= (cdr pair) 999))
+      max)
+     ((palindromic? (pair-multi pair))
+      (pfpot3d-iter (three-digit-pair-next pair) (pair-multi pair)))
+     (else
+      (pfpot3d-iter (three-digit-pair-next pair) max))))
+  (pfpot3d-iter '(100 . 100) 10201))
+
+
+;(palindromic? 123454321)
+;(palindromic? 123445321)
+(define (palindromic? num)
+  (= (num-reverse num) num))
+
+
+(define (num-reverse num)
+  (define (num-reverse-iter ret pool)
+    (if (= pool 0)
+	ret
+	(num-reverse-iter
+	 (+ (* ret 10) (remainder pool 10))
+	 (quotient pool 10))))
+  (num-reverse-iter 0 num))
+
+
+;(pair-multi '(100 . 101))
+(define (pair-multi pair)
+  (* (car pair) (cdr pair)))
+
+
+;(three-digit-pair-next '(999 . 100))
+;(three-digit-pair-next '(998 . 100))
+(define (three-digit-pair-next pair)
+  (cond
+   ((>= (car pair) 999) (cons 100 (+ (cdr pair) 1)))
+   (else (cons (+ (car pair) 1) (cdr pair)))))
+
+
+(provide "util.palindromic")
